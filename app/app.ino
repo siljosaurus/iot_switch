6eef077773c635875b002d7f46e3a51022513187
Adrian Fjelle Evensen <guleadrian@gmail.com>
2017-09-07 18:41:46 +0200

    Formatting

--- app/app.ino
+++ app/app.ino
@@ -24,99 +24,86 @@ static char *pass;
 
 static int interval = INTERVAL;
 
-void blinkLED()
-{
-    digitalWrite(LED_PIN, HIGH);
-    delay(500);
-    digitalWrite(LED_PIN, LOW);
+void blinkLED() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
 }
 
-void initWifi()
-{
-    // Attempt to connect to Wifi network:
-    Serial.printf("Attempting to connect to SSID: %s.\r\n", ssid);
+void initWifi() {
+  // Attempt to connect to Wifi network:
+  Serial.printf("Attempting to connect to SSID: %s.\r\n", ssid);
 
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+  // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    // Get Mac Address and show it.
+    // WiFi.macAddress(mac) save the mac address into a six length array, but the endian may be different. The huzzah board should
+    // start from mac[0] to mac[5], but some other kinds of board run in the oppsite direction.
+    uint8_t mac[6];
+    WiFi.macAddress(mac);
+    Serial.printf("You device with MAC address %02x:%02x:%02x:%02x:%02x:%02x connects to %s failed! Waiting 10 seconds to retry.\r\n",
+                  mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], ssid);
     WiFi.begin(ssid, pass);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        // Get Mac Address and show it.
-        // WiFi.macAddress(mac) save the mac address into a six length array, but the endian may be different. The huzzah board should
-        // start from mac[0] to mac[5], but some other kinds of board run in the oppsite direction.
-        uint8_t mac[6];
-        WiFi.macAddress(mac);
-        Serial.printf("You device with MAC address %02x:%02x:%02x:%02x:%02x:%02x connects to %s failed! Waiting 10 seconds to retry.\r\n",
-                mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], ssid);
-        WiFi.begin(ssid, pass);
-        delay(10000);
-    }
-    Serial.printf("Connected to wifi %s.\r\n", ssid);
+    delay(10000);
+  }
+  Serial.printf("Connected to wifi %s.\r\n", ssid);
 }
 
-void initTime()
-{
-    time_t epochTime;
-    configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+void initTime() {
+  time_t epochTime;
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
 
-    while (true)
-    {
-        epochTime = time(NULL);
+  while (true) {
+    epochTime = time(NULL);
 
-        if (epochTime == 0)
-        {
-            Serial.println("Fetching NTP epoch time failed! Waiting 2 seconds to retry.");
-            delay(2000);
-        }
-        else
-        {
-            Serial.printf("Fetched NTP epoch time is: %lu.\r\n", epochTime);
-            break;
-        }
+    if (epochTime == 0) {
+      Serial.println("Fetching NTP epoch time failed! Waiting 2 seconds to retry.");
+      delay(2000);
     }
+    else {
+      Serial.printf("Fetched NTP epoch time is: %lu.\r\n", epochTime);
+      break;
+    }
+  }
 }
 
 static IOTHUB_CLIENT_LL_HANDLE iotHubClientHandle;
-void setup()
-{
-    pinMode(LED_PIN, OUTPUT);
+void setup() {
+  
+  pinMode(LED_PIN, OUTPUT);
 
-    initSerial();
-    delay(2000);
-    readCredentials();
+  initSerial();
+  delay(2000);
+  readCredentials();
 
-    initWifi();
-    initTime();
-    initSensor();
+  initWifi();
+  initTime();
+  initSensor();
 
-    /*
-     * AzureIotHub library remove AzureIoTHubClient class in 1.0.34, so we remove the code below to avoid
-     *    compile error
-    */
+  iotHubClientHandle = IoTHubClient_LL_CreateFromConnectionString(connectionString, MQTT_Protocol);
+  if (iotHubClientHandle == NULL) {
+    Serial.println("Failed on IoTHubClient_CreateFromConnectionString.");
+    while (1);
+  }
 
-    // initIoThubClient();
-    iotHubClientHandle = IoTHubClient_LL_CreateFromConnectionString(connectionString, MQTT_Protocol);
-    if (iotHubClientHandle == NULL)
-    {
-        Serial.println("Failed on IoTHubClient_CreateFromConnectionString.");
-        while (1);
-    }
-
-    IoTHubClient_LL_SetMessageCallback(iotHubClientHandle, receiveMessageCallback, NULL);
-    IoTHubClient_LL_SetDeviceMethodCallback(iotHubClientHandle, deviceMethodCallback, NULL);
-    IoTHubClient_LL_SetDeviceTwinCallback(iotHubClientHandle, twinCallback, NULL);
+  IoTHubClient_LL_SetMessageCallback(iotHubClientHandle, receiveMessageCallback, NULL);
+  IoTHubClient_LL_SetDeviceMethodCallback(iotHubClientHandle, deviceMethodCallback, NULL);
+  IoTHubClient_LL_SetDeviceTwinCallback(iotHubClientHandle, twinCallback, NULL);
 }
 
 static int messageCount = 1;
-void loop()
-{
-    if (!messagePending && messageSending)
-    {
-        char messagePayload[MESSAGE_MAX_LEN];
-        bool temperatureAlert = readMessage(messageCount, messagePayload);
-        sendMessage(iotHubClientHandle, messagePayload, temperatureAlert);
-        messageCount++;
-        delay(interval);
-    }
-    IoTHubClient_LL_DoWork(iotHubClientHandle);
-    delay(10);
+void loop() {
+  if (!messagePending && messageSending) {
+    
+    char messagePayload[MESSAGE_MAX_LEN];
+    bool temperatureAlert = readMessage(messageCount, messagePayload);
+    sendMessage(iotHubClientHandle, messagePayload, temperatureAlert);
+    messageCount++;
+    delay(interval);
+  }
+
+  
+  IoTHubClient_LL_DoWork(iotHubClientHandle);
+  delay(10);
 }
